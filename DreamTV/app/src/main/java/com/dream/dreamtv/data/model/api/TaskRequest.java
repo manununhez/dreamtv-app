28959806f7945c58cb8393d44d64525c3df88f76
Manuel Nunez <03.manu@gmail.com>
2019-09-10 03:44:01 +0200

    Task request parameter formatting by classes

---
+++ DreamTV/app/src/main/java/com/dream/dreamtv/data/model/api/TaskRequest.java
@@ -0,0 +1,49 @@
+package com.dream.dreamtv.data.model.api;
+
+import com.dream.dreamtv.data.model.Category;
+import com.dream.dreamtv.data.model.VideoDuration;
+
+public class TaskRequest {
+
+    private Category.Type mCategory;
+    private VideoDuration mVideoDuration;
+
+    public TaskRequest(Category.Type category, VideoDuration videoDuration) {
+        mCategory = category;
+        mVideoDuration = videoDuration;
+    }
+
+    public Category.Type getCategory() {
+        return mCategory;
+    }
+
+    public void setCategory(Category.Type mCategory) {
+        this.mCategory = mCategory;
+    }
+
+    public VideoDuration getVideoDuration() {
+        return mVideoDuration;
+    }
+
+    public void setVideoDuration(VideoDuration mVideoDuration) {
+        this.mVideoDuration = mVideoDuration;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TaskRequest that = (TaskRequest) o;
+
+        if (mCategory != that.mCategory) return false;
+        return mVideoDuration.equals(that.mVideoDuration);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = mCategory.hashCode();
+        result = 31 * result + mVideoDuration.hashCode();
+        return result;
+    }
+}
