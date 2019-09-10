5f658b80776679ac547b5e370abcf8d8d6f3c5e8
Manuel Nunez <03.manu@gmail.com>
2019-09-10 03:42:58 +0200

    Added View Model in Preferences in order to access to Preferences saved data

---
+++ DreamTV/app/src/main/java/com/dream/dreamtv/ui/preferences/PreferencesViewModel.java
@@ -0,0 +1,32 @@
+package com.dream.dreamtv.ui.preferences;
+
+import androidx.lifecycle.ViewModel;
+
+import com.dream.dreamtv.data.model.api.User;
+import com.dream.dreamtv.repository.AppRepository;
+
+public class PreferencesViewModel extends ViewModel {
+    private final AppRepository mRepository;
+
+    public PreferencesViewModel(AppRepository appRepository) {
+        mRepository = appRepository;
+
+    }
+
+
+    public User getUser() {
+        return mRepository.getUser();
+    }
+
+    public String getAudioLanguagePref() {
+        return mRepository.getAudioLanguagePref();
+    }
+
+    public String getInterfaceAppLanguage() {
+        return mRepository.getInterfaceAppLanguage();
+    }
+
+    public String getInterfaceMode() {
+        return mRepository.getInterfaceMode();
+    }
+}
